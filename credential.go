a1a701ab0631727b59987fffe1ab7904b297b11e
Daniel Gorbe <daniel@gorbe.io>
2024-08-01 13:21:10 +0200

    Add *sync.Mutex to ApiKey

--- credential.go
+++ credential.go
@@ -1,6 +1,9 @@
 package google
 
-import "math/rand"
+import (
+	"math/rand"
+	"sync"
+)
 
 type Credential interface {
 	Token() (string, error)
@@ -10,23 +13,27 @@ type ApiKey struct {
 	keys   []string
 	next   int
 	random bool
+	m      *sync.Mutex
 }
 
 func NewApiKey(key string) *ApiKey {
-	return &ApiKey{keys: []string{key}, random: false}
+	return &ApiKey{keys: []string{key}, random: false, m: new(sync.Mutex)}
 }
 
 func RandomApiKeys(keys ...string) *ApiKey {
-	return &ApiKey{keys: keys, random: true}
+	return &ApiKey{keys: keys, random: true, m: new(sync.Mutex)}
 }
 
 func RotatingApiKeys(keys ...string) *ApiKey {
-	return &ApiKey{keys: keys, random: false}
+	return &ApiKey{keys: keys, random: false, m: new(sync.Mutex)}
 }
 
 // Token returns the API key.
 // Renturns a random API key if multiple keys added with [NewApiKeys].
 func (k *ApiKey) Token() (string, error) {
+
+	k.m.Lock()
+	defer k.m.Unlock()
 
 	if len(k.keys) == 0 {
 		return "", nil
